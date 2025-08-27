1a13cdbdf59b957bd7a3678e339bbaaf30806f2e
Ronak <ronakrathod0022@gmail.com>
2025-08-27 16:34:11 +0530

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl bg-blue-500'>Ronak and His aims</h1>
+    </>
+  )
+}
+
+export default App
